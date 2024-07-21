a4f1cdf54d0dc10f15fbfd9cbaf29d3734884a79
Dmitry Markevich <mdv.dop@gmail.com>
2024-07-21 11:53:04 +0300

    Added web-browser

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,20 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { useState } from 'react';
+import { Image, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { openBrowserAsync } from 'expo-web-browser';
 
-import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  const [url, setUrl] = useState<string>('');
+  const [result, setResult] = useState<unknown>(null);
+
+  const handleOpenPress = async () => {
+    let result = await openBrowserAsync(url);
+    setResult(result);
+  };
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -14,37 +23,29 @@ export default function HomeScreen() {
           source={require('@/assets/images/partial-react-logo.png')}
           style={styles.reactLogo}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">Built-in Browser Runner</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
+        <ThemedText type="subtitle">Step 1: Enter the URL</ThemedText>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={setUrl}
+          autoCapitalize="none"
+          autoComplete="off"
+          autoCorrect={false}
+        />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
+        <ThemedText type="subtitle">
+          Step 2: Open it in built-in browser
         </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+        <TouchableOpacity style={styles.btn} onPress={handleOpenPress}>
+          <ThemedText style={styles.btnTxt}>Open</ThemedText>
+        </TouchableOpacity>
+        <ThemedText>{result ? JSON.stringify(result) : ''}</ThemedText>
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -66,5 +67,25 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: 'absolute',
+  },
+  textInput: {
+    borderColor: '#bec3c9',
+    borderWidth: 1,
+    borderRadius: 3,
+    padding: 3,
+  },
+  btn: {
+    width: 200,
+    height: 40,
+    alignSelf: 'center',
+    backgroundColor: '#06bcee',
+    borderRadius: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnTxt: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '500',
   },
 });
